32ee00ebcb47df3107a68d0170208f112ef13626
Milos1988 <vojvoda1988@gmail.com>
2023-10-27 00:33:57 +0200

    Finish

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const app = require("express")();
 const cors = require("cors");
 const server = require("http").createServer(app);
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 const sendMail = require("./services/mailServices");
 dotenv.config();
-const portNumber = 5500|| 6000;
+const portNumber = 5500 || 6000;
 
 app.use(cors());
 const io = require("socket.io")(server, {
@@ -16,28 +16,24 @@ const io = require("socket.io")(server, {
 
 io.on("connection", (socket) => {
   console.log("User connect", socket.id);
-    socket.on("enterRoom", (data) => {
-      if(!data.query){
-        socket.join(data.room)
-        sendMail(data.room, data.ip)
-      } else{
-        socket.join(Number(data.query))
-      }   
-       
-      
-    });
-    socket.on("sendMsg", (data) => {
-      socket.to(data.room).emit("receiveMessage", data.msg);
-    });
-  
-    socket.on("disconnect", () => {
-      console.log("User Disconnected", socket.id);
-    });
+  socket.on("enterRoom", (data) => {
+    if (!data.query) {
+      socket.join(data.room);
+      sendMail(data.room, data.ip);
+    } else {
+      socket.join(Number(data.query));
+    }
   });
-
+  socket.on("sendMsg", (data) => {
+    socket.to(data.room).emit("receiveMessage", data.msg);
+  });
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
 
 server.listen(portNumber, (error) => {
-    error
-      ? console.log("Error on server start")
-      : console.log(`Server is running on port ${portNumber}`);
-  });
+  error
+    ? console.log("Error on server start")
+    : console.log(`Server is running on port ${portNumber}`);
+});
