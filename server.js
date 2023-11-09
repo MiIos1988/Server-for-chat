e19c8e30410607d4c0ef1d9994c3a8b6a989f051
Milos1988 <vojvoda1988@gmail.com>
2023-11-10 00:34:56 +0100

    Change address for cors

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const room = new Map();
 app.use(cors());
 const io = require("socket.io")(server, {
   cors: {
-    origin: "https://sudimacmilos.vercel.app/",
+    origin: "https://sudimacmilos.vercel.app",
     methods: ["GET", "POST"],
   },
 });
