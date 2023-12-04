6510b0934dcfa9cbe5fd9035766c7f5ad26462ba
Milos1988 <vojvoda1988@gmail.com>
2023-12-04 23:55:35 +0100

    Add Current time

--- services/mailServices.js
+++ services/mailServices.js
@@ -1,5 +1,6 @@
 const nodemailer = require("nodemailer");
 const dotenv = require("dotenv");
+const moment = require('moment-timezone');
 
 dotenv.config();
 
@@ -11,7 +12,8 @@ const transporter = nodemailer.createTransport({
     }
 })
 const sendMail = function (room, ip) {
-    console.log(ip.data.country_name)
+    console.log(ip.data.country_name);
+    const currentTimeInSerbia = moment.tz(new Date(), 'Europe/Belgrade').format('YYYY-MM-DD HH:mm:ss');
     let mailOptions = {
         from: process.env.EMAIL_USERNAME , 
         to: process.env.EMAIL_USERNAME, 
@@ -20,6 +22,7 @@ const sendMail = function (room, ip) {
         https://sudimacmilos.vercel.app/?room=${room}
             <div>
                 <p>Ip address: ${ip.data.ip}</p>
+                <p><strong>Current Time in Serbia:</strong> ${currentTimeInSerbia}</p>
                 <p>City: ${ip.data.city}  - Country: ${ip.data.country_name}</p>
                 <p>Org: ${ip.data.org}</p>
                 <p>Map: </p> https://www.google.com/maps?q=${ip.data.latitude},${ip.data.longitude}
