85ce5d9365a529d877fbcfbb2ff565dd63916252
Milos1988 <vojvoda1988@gmail.com>
2024-04-01 23:36:31 +0200

    Corrected code

--- server.js
+++ server.js
@@ -31,7 +31,6 @@ io.on("connection", (socket) => {
       .emit("receiveMessage", { msg: data.msg, author: data.author });
   });
   socket.on("chat-visibility", (data) => {
-    console.log("chat-visibility", data.chat);
     socket.to(data.room).emit("showChat", data.chat);
   });
   socket.on("disconnect", () => {
